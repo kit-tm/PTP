69650291ccfaf4ef60f20f2717a9559706de3e71
Simeon Andreev <simeon.andreev@student.kit.edu>
2014-10-19 13:55:23 +0200

    updated pingpong test to clean on exceptions

--- src/test/TorP2PPingPongTest.java
+++ src/test/TorP2PPingPongTest.java
@@ -123,84 +123,93 @@ public class TorP2PPingPongTest {
 	 * @throws IllegalArgumentException Propagates any IllegalArgumentException thrown by the configuration.
 	 * @throws IOException Propagates any IOException received by the API, or thrown by the console read operations.
 	 */
-	public static void main(String[] args) throws IllegalArgumentException, IOException {
-		// Create an API wrapper object.
-		final TorP2P client = new TorP2P();
+	public static void main(String[] args) {
+		TorP2P client = null;
 
-        // Set the timer start for the hidden service creation measurement.
-		long start = System.currentTimeMillis();
-		final String identifier = client.GetIdentifier();
-		final long duration = System.currentTimeMillis() - start;
+		try {
+			// Create an API wrapper object.
+			client = new TorP2P();
 
-		// Output the created hidden service identifier.
-		System.out.println("Created hidden service.");
-		System.out.println("Identifier : " + identifier);
-		System.out.println("Port       : " + client.GetLocalPort());
+	        // Set the timer start for the hidden service creation measurement.
+			long start = System.currentTimeMillis();
+			final String identifier = client.GetIdentifier();
+			final long duration = System.currentTimeMillis() - start;
 
-		// Create a reader for the console input.
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// Ask for the destination hidden service identifier.
-        System.out.print("Enter hidden service identifier:");
-        final String destination = br.readLine();
+			// Output the created hidden service identifier.
+			System.out.println("Created hidden service.");
+			System.out.println("Identifier : " + identifier);
+			System.out.println("Port       : " + client.GetLocalPort());
 
-       	// Connect to the destination hidden service and port by sending a dummy message.
-		System.out.println("Connecting client.");
-		TorP2P.SendResponse response = client.SendMessage("", destination, 120 * 1000);
-		// Check if something broke when sending.
-		if (response != TorP2P.SendResponse.SUCCESS)
-			throw new IOException("Could not send greeting message in the given timeout.");
-		// Set the timer start for the hidden service availability measurement.
-		final long available = System.currentTimeMillis() - start;
+			// Create a reader for the console input.
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			// Ask for the destination hidden service identifier.
+	        System.out.print("Enter hidden service identifier:");
+	        final String destination = br.readLine();
 
-		// The maximum number of PING-PONGs.
-		final int max = 50;
-		// Create the listener and set it.
-		final MyListener listener = new MyListener(client, destination, max);
-		client.SetListener(listener);
+	       	// Connect to the destination hidden service and port by sending a dummy message.
+			System.out.println("Connecting client.");
+			TorP2P.SendResponse response = client.SendMessage("", destination, 120 * 1000);
+			// Check if something broke when sending.
+			if (response != TorP2P.SendResponse.SUCCESS)
+				throw new IOException("Could not send greeting message in the given timeout.");
+			// Set the timer start for the hidden service availability measurement.
+			final long available = System.currentTimeMillis() - start;
+
+			// The maximum number of PING-PONGs.
+			final int max = 50;
+			// Create the listener and set it.
+			final MyListener listener = new MyListener(client, destination, max);
+			client.SetListener(listener);
 
 
-		// Connected, ask if a message should be sent.
-		System.out.println("Connected.");
-		while (true) {
-			// Ask the user.
-			System.out.println("Send message? (yes/no)");
-			// Read the user input.
-			String line = br.readLine();
+			// Connected, ask if a message should be sent.
+			System.out.println("Connected.");
+			while (true) {
+				// Ask the user.
+				System.out.println("Send message? (yes/no)");
+				// Read the user input.
+				String line = br.readLine();
 
-			// If the input is "yes", ask for a message to send.
-			if (line.equals("yes")) {
-				System.out.println("Enter message to send:");
-				String message = br.readLine();
-				System.out.println("Sending first message.");
-				// Send the initial message.
-				listener.send(message, false);
-				break;
-			// If the input is "no" proceed without sending a message.
-			} else if (line.equals("no"))
-				break;
-			// Only accept "yes" or "no".
-		}
-
-		// Wait until the maximum number of PING-PONGs is reached.
-		System.out.println("Sleeping.");
-		while (listener.count() < max) {
-			try {
-				// Sleep.
-				Thread.sleep(5 * 1000);
-			} catch (InterruptedException e) {
-				System.out.println("Main thread interrupted.");
+				// If the input is "yes", ask for a message to send.
+				if (line.equals("yes")) {
+					System.out.println("Enter message to send:");
+					String message = br.readLine();
+					System.out.println("Sending first message.");
+					// Send the initial message.
+					listener.send(message, false);
+					break;
+				// If the input is "no" proceed without sending a message.
+				} else if (line.equals("no"))
+					break;
+				// Only accept "yes" or "no".
 			}
-		}
 
-		// Show time measurements.
-		System.out.println("Displaying running time statistics.");
-		System.out.println("\t To create HS         : " + duration + " ms.");
-		System.out.println("\t Wait until reachable : " + available + " ms.");
-		if (listener.sent() > 0)
-			System.out.println("\t RTT                  : " + listener.duration() / listener.sent() + " ms.");
+			// Wait until the maximum number of PING-PONGs is reached.
+			System.out.println("Sleeping.");
+			while (listener.count() < max) {
+				try {
+					// Sleep.
+					Thread.sleep(5 * 1000);
+				} catch (InterruptedException e) {
+					System.out.println("Main thread interrupted.");
+				}
+			}
+
+			// Show time measurements.
+			System.out.println("Displaying running time statistics.");
+			System.out.println("\t To create HS         : " + duration + " ms.");
+			System.out.println("\t Wait until reachable : " + available + " ms.");
+			if (listener.sent() > 0)
+				System.out.println("\t RTT                  : " + listener.duration() / listener.sent() + " ms.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
 
 		// Done, exit.
 		System.out.println("Exiting client.");
+		if (client != null)
 		client.Exit();
 	}
 
