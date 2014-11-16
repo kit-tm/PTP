e9ef0f012f37b6b719a8ce9e035115aa32d12ab3
Simeon <Simeon@HOTRIDE>
2014-11-16 13:12:13 +0100

    added shutdown hook to Tor manager cleanup

--- src/thread/TorManager.java
+++ src/thread/TorManager.java
@@ -166,6 +166,15 @@ public class TorManager extends Manager {
 				channel.close();
 				raf.close();
 
+				// If the application is terminated nicely, check if the Tor process needs to be stopped.
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+
+					public void run() {
+						stoptor();
+					}
+
+				});
+
 				// Wait for the Tor manager ports file to appear and read it.
 				waittor();
 			} catch (IOException e) {
@@ -293,7 +302,7 @@ public class TorManager extends Manager {
 	 * Stops the API Tor process, if no other API is using the process.
 	 */
 	private void stoptor() {
-		logger.log(Level.INFO, "Tor manager closing Tor process.");
+		logger.log(Level.INFO, "Tor manager checking if Tor process should be closed.");
 		try {
 			logger.log(Level.INFO, "Tor manager acquiring lock on Tor manager lock file.");
 			RandomAccessFile raf = new RandomAccessFile(lockFile, Constants.readwriterights);
