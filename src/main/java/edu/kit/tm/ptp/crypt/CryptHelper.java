8319e8bd270f3a85ac558226a42e88cdf24c2a45
Timon Hackenjos <timon.hackenjos@student.kit.edu>
2016-05-29 09:36:49 +0200

    Removed unused import

--- src/main/java/edu/kit/tm/ptp/crypt/CryptHelper.java
+++ src/main/java/edu/kit/tm/ptp/crypt/CryptHelper.java
@@ -13,7 +13,6 @@ import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
