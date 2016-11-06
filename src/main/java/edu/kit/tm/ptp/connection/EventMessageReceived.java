6432db25326a205a19642e9dabe42b310d8b2dbd
Timon Hackenjos <timon.hackenjos@student.kit.edu>
2016-11-06 17:41:17 +0100

    Ignore findbugs error

--- src/main/java/edu/kit/tm/ptp/connection/EventMessageReceived.java
+++ src/main/java/edu/kit/tm/ptp/connection/EventMessageReceived.java
@@ -3,6 +3,7 @@ package edu.kit.tm.ptp.connection;
 import java.util.logging.Level;
 
 import edu.kit.tm.ptp.channels.MessageChannel;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /**
  * Class for the event that a message was received.
@@ -12,6 +13,9 @@ public class EventMessageReceived extends Event {
   private byte[] data;
   private MessageChannel source;
 
+  @SuppressFBWarnings(value = "EI_EXPOSE_REP2",
+      justification = "MessageChannel uses a new buffer for each message" +
+          "and doesn't alter them after reception.")
   public EventMessageReceived(ConnectionManager manager, byte[] data, MessageChannel source) {
     super(manager);
 
